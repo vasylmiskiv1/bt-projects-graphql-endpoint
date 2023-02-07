59fd670ab5281265cca735af9c1b318bd9081e9e
Vasyl Miskiv <vasylmiskiv@gmail.com>
2023-02-08 01:56:08 +0200

    change cors options

--- src/index.js
+++ src/index.js
@@ -14,6 +14,13 @@ connectDB();
 
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(
   "/graphql",
   graphqlHTTP({
